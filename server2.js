302e320e181c0d239e66150179d93a44fe5b3f7a
fendicruz-beep <fendijermaecruz@gmail.com>
2026-07-24 15:07:58 +0800

    Add files via upload

--- server2.js
+++ server2.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var app = express();
  
-app.use(express.static('public'));
- 
 app.get('/', function (req, res) {
-  res.send('You have successfully created second app!');
+  res.send('You have successfully created your second app!');
 })
  
-var server = app.listen(8081, function () {
+var server = app.listen(2001, function () {
   var host = server.address().address
   var port = server.address().port
+ 
   console.log("Example app listening at http://%s:%s", host, port)
 })
